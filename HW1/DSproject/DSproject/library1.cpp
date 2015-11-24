8cbafa1118d772817836899c0fc8120f54dddb97
Shakedor <light.skep@gmail.com>
2015-11-24 20:47:25 +0200

    started adding tests

---
+++ HW1/DSproject/DSproject/library1.cpp
@@ -0,0 +1,11 @@
+#include "library1.h"
+#include "DS.h"
+
+
+void* Init(){	return new(DS);}StatusType AddTrainer(void *DS, int trainerID);StatusType CatchPokemon(void *DS, int pokemonID, int trainerID, int level);StatusType FreePokemon(void *DS, int pokemonID);StatusType LevelUp(void *DS, int pokemonID, int levelIncrease);StatusType GetTopPokemon(void *DS, int trainerID, int *pokemonID);StatusType GetAllPokemonsByLevel(void *DS, int trainerID, int **pokemons, int numOfPokemon);StatusType EvolvePokemon(void *DS, int pokemonID, int evolvedID);
+StatusType UpdateLevels(void *DS, int stoneCode, int stoneFactor);
+void Quit(void **DS){
+	if (DS != nullptr){
+		delete DS;
+	}
+}
