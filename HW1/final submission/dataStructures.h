e92266bd2a2849bf041772d7100a5b2e3e9a613e
Shakedor <light.skep@gmail.com>
2015-12-04 19:29:54 +0200

    project submitted

---
+++ HW1/final submission/dataStructures.h
@@ -0,0 +1,121 @@
+#ifndef DAT_STRUCT
+#define DAT_STRUCT
+#include <iostream>
+
+namespace dataStructures{
+	
+	template<class data>
+	class smart_pointer;
+	template<class data>
+	std::ostream& operator<<(std::ostream& os, smart_pointer<data>& sp);
+
+	class dataSturctExceptions{};
+	class failureExceptions : public dataSturctExceptions {};
+	class dataAlreadyExists : public failureExceptions {};
+	class dataDoesNotExist : public failureExceptions {};
+	class stucountertIsEmpty : public failureExceptions {};
+	class sizeOverFlow : public failureExceptions {};
+	class sturctIsEmpty : public dataDoesNotExist{};
+
+	class counter{
+		int number;
+
+	public:
+		counter() :number(0){}
+
+		void increment(){
+			number++;
+		}
+
+		void decrement(){
+			--number;
+		}
+
+		int getNumber(){
+			return number;
+		}
+
+	};
+
+
+	template<class data>
+	class smart_pointer{
+		data* dataPointer;
+		counter* Counter;
+	public:
+		smart_pointer() : dataPointer(NULL){
+			Counter = new counter();
+			Counter->increment();
+		}
+
+		smart_pointer(data* dataP) : dataPointer(dataP){
+			 
+			Counter = new counter();
+			Counter->increment();
+		}
+
+		smart_pointer(const smart_pointer<data>& sp) : dataPointer(sp.dataPointer), Counter(sp.Counter)
+		{
+			Counter->increment();
+		}
+
+		~smart_pointer()
+		{
+			Counter->decrement();
+			if (Counter->getNumber() == 0)
+			{
+				delete dataPointer;
+				delete Counter;
+			}
+		}
+
+		data& operator* ()
+		{
+			return *dataPointer;
+		}
+
+		data* operator-> ()
+		{
+			return dataPointer;
+		}
+
+		smart_pointer<data>& operator = (const smart_pointer<data>& sp)
+		{
+			
+			if (this != &sp) // avoid assigning to itself
+			{
+				//destruction protocol for old data
+				Counter->decrement();
+
+				if (Counter->getNumber() == 0)
+				{
+					delete dataPointer;
+					delete Counter;
+				}
+
+				//copy fields and increment
+				dataPointer = sp.dataPointer;
+				Counter = sp.Counter;
+				Counter->increment();
+			}
+			return *this;
+		}
+
+
+
+	};
+
+
+	template<class data>
+	class spPrint{
+	public:
+		spPrint(){}
+		void operator() (smart_pointer<data>& point){
+			(*point).print();
+		}
+	};
+
+}
+
+
+#endif  /* DAT_STRUCT */
