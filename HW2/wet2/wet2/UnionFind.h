07d34eb608103f4394b45146c084de14a76b750e
Shakedor <light.skep@gmail.com>
2015-12-27 11:41:13 +0200

    Adding HW2, implemented dynamic array

---
+++ HW2/wet2/wet2/UnionFind.h
@@ -0,0 +1,33 @@
+#ifndef UNIOUN_FIND_
+#define UNIOUN_FIND_
+
+#include <cstdlib>
+#include <cassert>
+#include "dataStructures.h"
+#include <cmath>
+
+
+/*
+a template implementation of a union find disjoint set.
+
+member represents data of members of sets 
+set represents data of a sinle set
+
+classes area ssumed to have null argument constructors.
+
+*/
+
+
+template<class set, class member, int size>
+class UnionFind{
+	int n;
+
+public:
+
+
+};
+
+
+
+
+#endif UNIOUN_FIND_
---
+++ HW2/wet2/wet2/dataStructures.h
@@ -0,0 +1,182 @@
+#ifndef DAT_STRUCT
+#define DAT_STRUCT
+#include <iostream>
+
+namespace dataStructures{
+	
+	template<class data>
+	class smart_pointer;
+	template<class data>
+	std::ostream& operator<<(std::ostream& os, smart_pointer<data>& sp);
+
+	class dataSturctExceptions{};
+	class failureExceptions : public dataSturctExceptions {};
+	class dataAlreadyExists : public failureExceptions {};
+	class dataDoesNotExist : public failureExceptions {};
+	class stucountertIsEmpty : public failureExceptions {};
+	class sizeOverFlow : public failureExceptions {};
+	class sturctIsEmpty : public dataDoesNotExist{};
+
+	class counter{
+		int number;
+
+	public:
+		counter() :number(0){}
+
+		void increment(){
+			number++;
+		}
+
+		void decrement(){
+			--number;
+		}
+
+		int getNumber(){
+			return number;
+		}
+
+	};
+
+
+	template<class data>
+	class smart_pointer{
+		data* dataPointer;
+		counter* Counter;
+	public:
+		smart_pointer() : dataPointer(NULL){
+			Counter = new counter();
+			Counter->increment();
+		}
+
+		smart_pointer(data* dataP) : dataPointer(dataP){
+			 
+			Counter = new counter();
+			Counter->increment();
+		}
+
+		smart_pointer(const smart_pointer<data>& sp) : dataPointer(sp.dataPointer), Counter(sp.Counter)
+		{
+			Counter->increment();
+		}
+
+		~smart_pointer()
+		{
+			Counter->decrement();
+			if (Counter->getNumber() == 0)
+			{
+				delete dataPointer;
+				delete Counter;
+			}
+		}
+
+		data& operator* ()
+		{
+			return *dataPointer;
+		}
+
+		data* operator-> ()
+		{
+			return dataPointer;
+		}
+
+		smart_pointer<data>& operator = (const smart_pointer<data>& sp)
+		{
+			
+			if (this != &sp) // avoid assigning to itself
+			{
+				//destruction protocol for old data
+				Counter->decrement();
+
+				if (Counter->getNumber() == 0)
+				{
+					delete dataPointer;
+					delete Counter;
+				}
+
+				//copy fields and increment
+				dataPointer = sp.dataPointer;
+				Counter = sp.Counter;
+				Counter->increment();
+			}
+			return *this;
+		}
+
+
+
+	};
+
+
+	template<class data>
+	class spPrint{
+	public:
+		spPrint(){}
+		void operator() (smart_pointer<data>& point){
+			(*point).print();
+		}
+	};
+
+
+	template<class data>
+	class dynamicArray{
+		int size;
+		int counter;
+		data* arr;
+	public:
+
+		data& operator[](int idx){
+			if (idx < 0 || idx >= size){
+				throw sizeOverFlow();
+			}
+
+			return arr[idx];
+		}
+
+		dynamicArray(int initSize=10):size(initSize),counter(0){
+			arr = new data[size];
+			for (int i = 0; i < size; i++){
+				arr[i] = NULL;
+			}
+		}
+
+		dynamicArray(dynamicArray<data>& old) :counter(0){
+			size = old.size;
+			arr = new data[size];
+			for (int i = 0; i < size; i++){
+				arr[i] = old[i];
+			}
+		}
+
+		~dynamicArray(){
+			delete[] arr;
+		}
+
+		int getSize(){
+			return size;
+		}
+
+		void reSize(int newSize){
+			if (newSize <= 0){
+				throw sizeOverFlow();
+			}
+			data* temparr = new data[newSize];
+			int minSize = (size < newSize) ? size : newSize;
+
+			for (int i = 0; i < minSize < i++){
+				temparr[i] = arr[i];
+			}
+			for (int i = minSize; i < newSize; i++){
+				temparr[i] = NULL;
+			}
+
+			delete[] arr;
+			arr = temparr;
+			size = newSize;
+		}
+
+
+	};
+
+}
+
+
+#endif  /* DAT_STRUCT */
